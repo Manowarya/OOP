b56433ed07e5d702736c6c5bf37bc41f7aff73da
Manowarya <bobrasya@gmail.com>
2021-11-21 21:50:54 +0300

    Add att2

--- att2/build.bat
+++ att2/build.bat
@@ -1,4 +1,1 @@
-javac --module-path ./lib;./lib/javafx-sdk-17.0.0.1/lib --add-modules javafx.controls,javafx.fxml,javafx.graphics,slf4j.api -sourcepath ./src -d bin src/edu/csf/oop/java/seaBattle/Game/Main.java src/module-info.java
-cd bin
-jar cvfe ../SeaBattle.jar edu.csf.oop.java.seaBattle.Main edu/
-cd ..
+mvn compile
--- att2/run.bat
+++ att2/run.bat
@@ -1,1 +1,1 @@
-java --module-path ./lib;./lib/javafx-sdk-17.0.0.1/lib --add-modules javafx.controls,javafx.fxml,javafx.graphics,slf4j.api -jar SeaBattle.jar
+mvn exec:java -Dexec.mainClass="edu.csf.oop.java.seaBattle.Main"
